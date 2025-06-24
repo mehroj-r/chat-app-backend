08910d9173df852f085e8c3c4cec89681ecda4d5
MEHROJ <mekhrojmajidov@gmail.com>
2025-06-24 16:22:08 +0500

    fix: wsclient method was not awaited

--- src/core/websocket/client.py
+++ src/core/websocket/client.py
@@ -1,13 +1,14 @@
-class WSClient:
+class WSClient():
+    """Provides higher level abstraction over channel layer operations like sending messages and managing groups."""
 
     def __init__(self, channel_layer=None) -> None:
         self.channel_layer = channel_layer
 
     async def add_to_group(self, group: str, channel: str) -> None:
-        self.channel_layer.group_add(group, channel)
+        await self.channel_layer.group_add(group, channel)
 
     async def remove_from_group(self, group: str, channel: str) -> None:
-        self.channel_layer.group_discard(group, channel)
+        await self.channel_layer.group_discard(group, channel)
 
     async def send_to_group(self, group: str, event: dict) -> None:
         await self.channel_layer.group_send(group, event)
